a9d956c766102620bb404bfb798db72269dfe6e2
MisterMChem <mistermchem@gmail.com>
2016-07-21 15:17:07 -0400

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,44 @@
+$( document ).ready(function() {
+	login();
+});
+
+function userSignedIn(user) {
+	$("#loginContainer").hide();
+	$("#mainContainer").show();
+
+}
+
+function login() {
+	console.log("test");
+	var provider = new firebase.auth.GoogleAuthProvider();
+	var user = firebase.auth().currentUser;
+
+	if (user) {
+		console.log("signed in");
+  		// User is signed in.
+  		userSignedIn(user);
+	} else {
+  		// No user is signed in.
+  		$(".container").hide();
+  		$("#loginContainer").show();
+  		firebase.auth().signInWithPopup(provider).then(function(result) {
+			// This gives you a Google Access Token. You can use it to access the Google API.
+			var token = result.credential.accessToken;
+			// The signed-in user info.
+			var user = result.user;
+			userSignedIn(user);
+			// ...
+		}).catch(function(error) {
+		  	// Handle Errors here.
+		  	var errorCode = error.code;
+		  	var errorMessage = error.message;
+		  	// The email of the user's account used.
+		  	var email = error.email;
+		  	// The firebase.auth.AuthCredential type that was used.
+		  	var credential = error.credential;
+		  	// ...
+		});
+
+	}
+
+}
