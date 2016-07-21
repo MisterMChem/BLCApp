6e0b924c414606e69ae34c0ea91fa3b42a586547
MisterMChem <mistermchem@gmail.com>
2016-07-21 18:07:37 -0400

    Add files via upload

---
+++ js/attend.js
@@ -0,0 +1,38 @@
+$( document ).ready(function() {
+	getAttendees();
+});
+
+function getAttendees(){
+	firebase.database().ref('Users/').once('value').then(function(snapshot){
+		var UsersObject = snapshot.val();
+    	var keys = Object.keys(UsersObject);
+    	var currentUser;
+    	for (var i = 0; i < keys.length; i++) {
+    		var currentUser = UsersObject[keys[i]];
+    		var row = document.createElement("row");
+    		$(row).addClass("row userBox");
+    		var col1 = document.createElement("div");
+    		$(col).addClass("col-lg-3");
+    		$(row).append(col1);
+    		var col2 = document.createElement("div");
+    		$(col2).addClass("col-lg-9");
+    		$(row).append(col2);
+    		var image = document.createElement("img");
+    		$(image).attr("src", currentUser.photo);
+    		$(image).addClass("listPhoto");
+    		$(col1).append(image);
+    		var nameText = document.createElement("h3");
+    		nameText.innerHTML = currentUser.fullname;
+    		$(col2).append(nameText);
+    		$(nameText).addClass("listName");
+    		var detailText = document.createElement("p");
+    		detailText.innerHTML = currentUser.loc;
+    		$(detailText).addClass("listDetail");
+    		$(col2).append(detailText);
+    		$("#attendRow").append(row);
+    	}
+
+
+
+	});
+}
